ee61cd496a9f397fea62d3755ac2b6d8234da27c
Andrea Cabrera <3774322450101@ingenieria.usac.edu.gt>
2022-12-12 14:48:20 -0600

    Inicio Proteus

---
+++ Arduino/codigo.ino/codigo.ino.ino
@@ -0,0 +1,22 @@
+#include <LiquidCrystal.h>
+
+
+#define COLS 16
+#define ROWS 4
+#define VELOCIDAD 300
+
+LiquidCrystal lcd(2, 3, 8, 9, 10, 11);
+
+void setup()
+{
+  lcd.begin(COLS,ROWS);
+  lcd.print("INFORMACION ----");
+}
+
+void loop()
+{
+ 
+  
+  
+   
+}
